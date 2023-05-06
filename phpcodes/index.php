4bec077e24a180e52fbcc0ab40303f34e664261f
TotoFromParadiso <s.makhatadze9@gmail.com>
2023-05-06 19:51:57 +0400

    debug

---
+++ phpcodes/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="product_list">
+        Product List
+    </h1>
+
+    <div class="buttons">
+    <button>
+        ADD
+    </button>
+
+    <button>
+        MASS DELETE
+    </button>
+    </div>
+    <HR>
+    <form action="index.php" method="post">
+ <div>
+    <label class="checkboxes" for="myCheckboxID">
+<input type="checkbox" name="JVC" value="JVC" > <br>
+JVC200123 <br>
+Acme DISC <br>
+1.00 $ <br>
+Size:700 MB <br>
+
+
+<input type="checkbox" name="JVC" value="JVC"> <br>
+JVC200123 <br>
+Acme DISC <br>
+1.00 $ <br>
+Size:700 MB <br>
+
+
+<input type="checkbox" name="JVC" value="JVC"> <br>
+JVC200123 <br>
+Acme DISC <br>
+1.00 $ <br>
+Size:700 MB <br>
+
+<input type="checkbox" name="JVC" value="JVC"> <br>
+JVC200123 <br>
+Acme DISC <br>
+1.00 $ <br>
+Size:700 MB <br>
+
+<input type="checkbox" name="GGW" value="GGW"> <br>
+GGWP0007 <br>
+War and Peace <br>
+20.00 $ <br>
+Weight: 2KG <br>
+
+<input type="checkbox" name="GGW" value="GGW"> <br>
+GGWP0007 <br>
+War and Peace <br>
+20.00 $ <br>
+Weight: 2KG <br>
+
+<input type="checkbox" name="GGW" value="GGW"> <br>
+GGWP0007 <br>
+War and Peace <br>
+20.00 $ <br>
+Weight: 2KG <br>
+
+<input type="checkbox" name="GGW" value="GGW"> <br>
+GGWP0007 <br>
+War and Peace <br>
+20.00 $ <br>
+Weight: 2KG <br>
+
+<input type="checkbox" name="TR1" value="TR1"> <br>
+TR120555 <br>
+Chair <br>
+40.00 $ <br>
+Dimension: 24x45x15 <br>
+
+<input type="checkbox" name="TR1" value="TR1"> <br>
+TR120555 <br>
+Chair <br>
+40.00 $ <br>
+Dimension: 24x45x15 <br>
+
+<input type="checkbox" name="TR1" value="TR1"> <br>
+TR120555 <br>
+Chair <br>
+40.00 $ <br>
+Dimension: 24x45x15 <br>
+
+<input type="checkbox" name="TR1" value="TR1"> <br>
+TR120555 <br>
+Chair <br>
+40.00 $ <br>
+Dimension: 24x45x15 <br>
+  </form>
+    </label>
+</div>
+<HR class="hr">
+
+<p class="second_p">Scandiweb Test assignment</p>
+    
+</body>
+</html>
+
+<?php
+
+?>
